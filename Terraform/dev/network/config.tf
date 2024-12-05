af0162183ffa74a5d267f53d0e46d87a800df092
amendafernando <mail2amenda@gmail.com>
2024-12-04 22:49:30 -0500

    update webserver seeder code

--- Terraform/dev/network/config.tf
+++ Terraform/dev/network/config.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "group3-terraform-state-dev"
-    key            = "network/terraform.tfstate"
+    key            = "dev/network/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
   }
--- Terraform/dev/webservers/main.tf
+++ Terraform/dev/webservers/main.tf
@@ -1,8 +1,10 @@
+
 module "webservers" {
   source             = "../../modules/webservers"
   env                = "dev"
-  vpc_id             = module.network.vpc_id
-  subnet_ids         = [module.network.public_subnet_ids[0], module.network.private_subnet_ids[0]]
+  vpc_id             = data.terraform_remote_state.network.outputs.vpc_id
+  subnet_ids         = [data.terraform_remote_state.network.outputs.public_subnet_ids[0], data.terraform_remote_state.network.outputs.public_subnet_ids[1]]
+  # subnet_ids         = data.terraform_remote_state.network.outputs.public_subnet_ids
   ami_id             = "ami-0453ec754f44f9a4a" 
   instance_type      = "t2.micro"
   key_name           = "my-keypair"
