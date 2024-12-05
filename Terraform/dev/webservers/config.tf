dd8e63514bce66707f316add7575c2d5d66d6779
amendafernando <mail2amenda@gmail.com>
2024-12-04 20:56:28 -0500

    webserver update commit

---
+++ Terraform/dev/webservers/config.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-east-1"
+}
+terraform {
+  backend "s3" {
+    bucket = "group3-terraform-state-dev"
+    key    = "dev/webservers/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+data "terraform_remote_state" "network" {
+  backend = "s3"
+
+  config = {
+    bucket         = "group3-terraform-state-dev"
+    key            = "dev/network/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
